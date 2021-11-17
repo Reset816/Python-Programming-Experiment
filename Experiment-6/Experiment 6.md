78ed6942ee18175d3cf82f3a5ea3526635936ee2
谭源 <kentertan12138@outlook.com>
2021-11-18 00:49:17 +0800

    finish experiment 6

---
+++ Experiment-6/Experiment 6.md
@@ -0,0 +1,26 @@
+# Experiment 6
+
+## 要求
+<img src="images/image-20211118004716303.png" alt="image-20211118004716303" style="zoom:50%;" />
+
+<img src="images/image-20211118004739160.png" alt="image-20211118004739160" style="zoom:50%;" />
+
+## 思路
+
+经过多次重构后，实现了题目要求的所有功能，并进行了异常处理。
+
+迭代过程可见 [Commits · Reset12138/Python-Programming-Experiment (github.com)](https://github.com/Reset12138/Python-Programming-Experiment/commits/master)
+
+对选项列表进行了动态处理，根据当前数据项情况动态显示、处理可用选项。
+
+此外，注意到了这个问题，[python - Why does "return list.sort()" return None, not the list? - Stack Overflow](https://stackoverflow.com/questions/7301110/why-does-return-list-sort-return-none-not-the-list)
+
+## 输入示例
+
+- ![image-20211118004113002](images/image-20211118004113002.png)
+
+- ![image-20211118004129461](images/image-20211118004129461.png)
+
+- ![image-20211118004538468](images/image-20211118004538468.png)
+
+  
--- Experiment-6/main.py
+++ Experiment-6/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 
 def get_filename():
@@ -13,21 +14,28 @@ def get_filename():
         if filename.endswith(".lst"):
             lst_list.append(filename)
 
-    if len(lst_list) == 0:
-        filename = input_filename(
-            display_text="No .lst files found. Please input a filename:"
-        )
-    elif len(lst_list) != 0:
-        for i in enumerate(lst_list, start=1):
-            print(i[0], ": ", i[1])
-        number = input(
-            "Please input the number of file to be loaded, 0 to give a file name for a new file: "
-        )
-        if number == "0":
-            filename = input_filename(display_text="Please input a filename:")
-        else:
-            filename = lst_list[int(number) - 1]
+    try:
+        if len(lst_list) == 0:
+            filename = input_filename(
+                display_text="No .lst files found. Please input a filename:"
+            )
+        elif len(lst_list) != 0:
+            for i in enumerate(lst_list, start=1):
+                print(i[0], ": ", i[1])
+            number = input(
+                "Please input the number of file to be loaded, 0 to give a file name for a new file: "
+            )
+            if number == "0":
+                filename = input_filename(display_text="Please input a filename: ")
+            elif int(number) < 0:
+                raise ValueError()
+            else:
+                filename = lst_list[int(number) - 1]
+        print("Choose filename: ", filename)
         return filename
+    except:
+        print(number, " is not vaild index")
+        sys.exit()
 
 
 def save_lines(filename, lines):
@@ -40,13 +48,48 @@ append_newline = lambda str: str if str.endswith("\n") else str + "\n"
 
 
 def get_lines(filename):
-    if os.path.exists(filename):
+    if os.path.isfile(filename):
         with open(filename, "r") as f:
             lines = f.readlines()
-            lines[-1] = append_newline(lines[-1])
+            if lines != []:
+                lines[-1] = append_newline(lines[-1])
     else:
         lines = []
-    return lines
+    return sorted(lines, key=str.lower)
+
+
+def options(len_of_lines, is_dirty):
+    list_of_options = ["A", "Q"]
+
+    while True:
+
+        print("[A]dd  ", end="")
+        if len_of_lines != 0:
+            print("[D]elete  ", end="")
+            list_of_options.append("D")
+        if is_dirty == True:
+            print("[S]ave  ", end="")
+            list_of_options.append("S")
+        print("[Q]uit  [a]: ", end="")
+
+        x = input()
+        if (x.upper() not in list_of_options) and x != "":
+            print("ERROR: invalid choice")
+            input("Press Enter to continue...")
+            continue
+        else:
+            return x
+
+
+def print_lines(lines):
+    if len(lines) == 0:
+        print("\nno items are in the list")
+
+    else:
+        print()
+        width = len(str(len(lines)))
+        for i in enumerate(lines, start=1):
+            print(f"{i[0]:>{width}d}: {i[1]}", end="")
 
 
 def main():
@@ -57,27 +100,13 @@ def main():
 
     while True:
 
-        if len(lines) == 0:
-            print("no items are in the list")
-
-            x = input("[A]dd  [Q]uit  [a]: ")
-            if x.upper() not in ["A", "Q"]:
-                print("ERROR: invalid choice--enter one of 'AaQq'")
-                input("Press Enter to continue...")
-                continue
-        else:
-            print()
-            for i in enumerate(lines, start=1):
-                print(i[0], ": ", i[1], end="")
-
-            x = input("[A]dd  [D]elete  [S]ave  [Q]uit  [a]: ")
-            if x.upper() not in ["A", "D", "S", "Q"]:
-                print("ERROR: invalid choice--enter one of 'AaDdSsQq'")
-                input("Press Enter to continue...")
-                continue
+        print_lines(lines)
+        is_dirty = lines != get_lines(filename)
+        x = options(len_of_lines=len(lines), is_dirty=is_dirty)
 
         if x.upper() == "A" or x == "":
             lines.append(append_newline(input("Add item: ")))
+            lines = sorted(lines, key=str.lower)
         elif x.upper() == "D":
             number = input("Delete item number (or 0 to cancel): ")
             if number == "0":
@@ -85,11 +114,13 @@ def main():
             lines.pop(int(number) - 1)
         elif x.upper() == "S":
             save_lines(filename, lines)
-            pass
         elif x.upper() == "Q":
-            confirm = input("Save unsaved changes (y/n) [y]: ")
-            if confirm.lower() == "y" or x == "":
-                save_lines(filename, lines)
+            if is_dirty == True:
+                confirm = input("Save unsaved changes (y/n) [y]: ")
+                if confirm.lower() == "y" or confirm == "":
+                    save_lines(filename, lines)
+                else:
+                    continue
             break
 
 
